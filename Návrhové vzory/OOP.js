c0c70328db71aaeea428bb3cd4ac3f19439231ce
DostalDaniel <74705552+DostalDaniel@users.noreply.github.com>
2022-02-21 20:15:46 +0100

    Delete Návrhové vzory directory

--- Návrhové vzory/OOP.js
+++
@@ -1,9 +0,0 @@
-// definice třídy, respektive konstruktoru
-var Osoba = function (jmeno, prijmeni) {
-    this.jmeno = jmeno;
-    this.prijmeni = prijmeni;
-}
-
-// vytvoreni instance tridy
-var pepa = new Osoba('pepa', 'novak');
-var jirka = new Osoba('jiri', 'prochazka');
--- Návrhové vzory/OOP.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-// funkce unset() se postará o zrušení přihlašovací session
-unset($_SESSION['prihlaseni']);
-// přesměrování na přihlašovací formulář
-header('Location: index.php');
-?>
